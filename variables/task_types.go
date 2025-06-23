b0c662cc6c9a91a98a680d9d6851ddfdfa427fc4
sinveria <perevalova_v01@bk.ru>
2025-06-23 11:51:58 +0300

    add variables grogram with different data types

---
+++ variables/task_types.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 23
+	var name string = "Viktoria"
+	var isStudent bool = true
+
+	height := 161.3
+
+	fmt.Println("Age:", age)
+	fmt.Println("Name:", name)
+	fmt.Println("Is student:", isStudent)
+	fmt.Println("Height:", height)
+}
